e1e8976ad25d75d2c58b1464b85372a43f10312b
Hyeon Kim <simnalamburt@gmail.com>
2014-03-28 02:39:20 +0900

    Added a write button to the new memo view
    * 글쓰기 버튼 추가

--- public/script.js
+++ public/script.js
@@ -4,4 +4,9 @@ $(document).ready(function() {
   $('.memo-new').click(function() {
     $('.memo-new > textarea').focus();
   });
+
+  $('.memo-write').click(function() {
+    var content = $('.memo.memo-new > textarea').val();
+    alert(content);
+  });
 });
