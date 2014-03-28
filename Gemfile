22b000386b14bf360f90d5178143e00516dc671b
Hyeon Kim <simnalamburt@gmail.com>
2014-03-28 11:44:19 +0900

    Added a database feature : Create

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Web
 gem 'sinatra', '>=1.4.4'
+gem 'sequel', '>=4.8.0'
--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require 'sinatra'
+require 'sequel'
+
+DB = Sequel.sqlite
+DB.create_table :articles do
+  primary_key :id
+  String :content
+end
+articles = DB[:articles]
 
 get '/' do
   send_file 'main.html'
 end
 
 post '/' do
-  return :ok
+  content = request.body.read
+  if !content.empty?
+    articles.insert content: content
+    return 200
+  else
+    return 400
+  end
 end
