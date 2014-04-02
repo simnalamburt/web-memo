259c7b9a95c56e095278c0230a01cfba07561393
Hyeon Kim <simnalamburt@gmail.com>
2014-04-02 18:09:14 +0900

    Added a database feature : Update

--- app.rb
+++ app.rb
@@ -1,19 +1,54 @@
 require 'sinatra'
 require 'sequel'
 
-DB = Sequel.sqlite('data.db')
-articles = DB[:articles]
 
+
+### Define Model
+DB = Sequel.sqlite('data.db')
+
+Sequel::Model.plugin(:schema)
+class Article < Sequel::Model
+  set_schema do
+    primary_key :id
+    String :content
+  end
+end
+
+unless Article.table_exists?
+  Article.create_table
+  Article.create do |entity|
+    entity.content = "Hello, World!\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in bibendum lorem. In viverra erat ipsum, id pretium urna vehicula ut. Proin vel quam ultricies, placerat urna ut, accumsan leo. Vivamus laoreet vestibulum nulla non dictum. Vestibulum non nisl quis risus dictum vestibulum hendrerit ut diam. Sed eget laoreet augue. Integer pulvinar massa scelerisque rhoncus consequat. Vivamus velit mi, suscipit bibendum tincidunt quis, pulvinar a ante."
+  end
+  Article.create do |entity|
+    entity.content = '"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."'
+  end
+end
+
+
+
+### Routes
 get '/' do
   erb :main
 end
 
 get '/articles/' do
-  @data = articles.all
+  @data = Article.all
   erb :articles
 end
 
 post '/articles/' do
-  articles.insert content: request.body.read
+  entity = Article.new
+  entity.content = request.body.read
+  entity.save
+  return 200
+end
+
+put '/articles/:id' do
+  DB.transaction do
+    entity = Article[params[:id]]
+    entity.lock!
+    entity.content = request.body.read
+    entity.save
+  end
   return 200
 end
