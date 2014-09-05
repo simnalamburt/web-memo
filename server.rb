64721264e7e6b9a9ef6e2f41af57f0f402e2e007
Hyeon Kim <simnalamburt@gmail.com>
2014-09-04 18:31:26 -0700

    Rewrite DB related codes of server.rb
    1. Sequel::Model => Sequel::Dataset
    2. Now server.rb receives client data as json

--- server.rb
+++ server.rb
@@ -1,24 +1,21 @@
 require 'sinatra'
 require 'sinatra/json'
+require 'json'
 require 'sequel'
 
 
 
 ### Model
 DB = Sequel.sqlite 'db/data.db'
+memos = DB[:memos]
 
-Sequel::Model.plugin :schema
-class Memo < Sequel::Model
-  set_schema do
+unless DB.table_exists? :memos
+  DB.create_table :memos do
     primary_key :id
     String :content
   end
-end
 
-unless Memo.table_exists?
-  Memo.create_table
-
-  [<<-A, <<-B].each { |seed| Memo.create content: seed }
+  [<<-A, <<-B].each { |content| memos.insert content: content }
 Hello, World!\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in bibendum lorem. In viverra erat ipsum, id pretium urna vehicula ut. Proin vel quam ultricies, placerat urna ut, accumsan leo. Vivamus laoreet vestibulum nulla non dictum. Vestibulum non nisl quis risus dictum vestibulum hendrerit ut diam. Sed eget laoreet augue. Integer pulvinar massa scelerisque rhoncus consequat. Vivamus velit mi, suscipit bibendum tincidunt quis, pulvinar a ante.
   A
 "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
@@ -29,35 +26,30 @@ end
 
 ### Controller
 get '/memos' do
-  json DB[:memos].all
+  json memos.all
 end
 
 post '/memos' do
-  content = params[:content]
+  request.body.rewind
+  content = JSON.parse(request.body.read)['content']
 
   # No empty memo
-  if content.empty?
-    return 400
-  end
+  return 400 if !content || content.empty?
 
-  Memo.create do |memo|
-    memo.content = content
-  end
+  memos.insert content: content
   200
 end
 
-put '/memos/:id' do
-  DB.transaction do
-    memo = Memo[params[:id]]
-    memo.lock!
-    memo.content = params[:content]
-    memo.save
-  end
+put '/memos/:id' do |id|
+  request.body.rewind
+  content = JSON.parse(request.body.read)['content']
+
+  memos.where(id: id).update content: content
   200
 end
 
-delete '/memos/:id' do
-  Memo[params[:id]].destroy
+delete '/memos/:id' do |id|
+  memos.where(id: id).delete
   200
 end
 
