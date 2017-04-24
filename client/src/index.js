4e4a13660115ef7d5c7aa5a1c7c49fefe32b61e1
Hyeon Kim <simnalamburt@gmail.com>
2017-04-24 14:00:17 +0900

    Separate google analytics code

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import angular from 'angular'
 import 'angular-elastic'
 import 'restangular'
+import './ga.js'
 
 import 'font-awesome/css/font-awesome.css'
 import 'normalize.css'
@@ -46,11 +47,3 @@ angular
 
   return $scope;
 }]);
-
-// Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-47724944-1', 'auto');
-ga('send', 'pageview');
