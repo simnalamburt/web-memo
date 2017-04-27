f87839dab14a56de9b88a500505d73efb6b84179
Hyeon Kim <simnalamburt@gmail.com>
2017-04-27 15:28:58 +0900

    Remove semicolons

--- client/src/index.ts
+++ client/src/index.ts
@@ -10,40 +10,40 @@ import './index.styl'
 angular
 .module('hyeonme', ['monospaced.elastic', 'restangular'])
 .controller('MemoCtrl', ['$scope', 'Restangular', ($scope, Restangular) => {
-  const all = Restangular.all('memos');
-  const memos = all.getList().$object;
-  const select = (i: number) => memos.find((memo: { id: number }) => memo.id === i);
+  const all = Restangular.all('memos')
+  const memos = all.getList().$object
+  const select = (i: number) => memos.find((memo: { id: number }) => memo.id === i)
 
-  $scope.memos = memos;
+  $scope.memos = memos
 
   $scope.create = () => {
-    if ($scope.new == null || $scope.new.content == null) { return; }
+    if ($scope.new == null || $scope.new.content == null) { return }
 
     return all.post($scope.new)
       .then((id: number) => {
-        $scope.new.id = id;
+        $scope.new.id = id
 
-        const elem = Restangular.restangularizeElement($scope.parentResource, $scope.new, 'memos');
-        memos.unshift(elem);
+        const elem = Restangular.restangularizeElement($scope.parentResource, $scope.new, 'memos')
+        memos.unshift(elem)
 
-        $scope.new = {};
-        return $scope.new;
+        $scope.new = {}
+        return $scope.new
       })
-      .catch(() => { throw Error('unimplemented'); });
-  };
+      .catch(() => { throw Error('unimplemented') })
+  }
 
   $scope.update = (i: number) => select(i).put()
-    .catch(() => { throw Error('unimplemented'); });
+    .catch(() => { throw Error('unimplemented') })
 
   $scope.delete = (i: number) => {
-    const memo = select(i);
+    const memo = select(i)
     return memo.remove()
       .then(() => {
-        const index = memos.indexOf(memo);
-        if (index !== -1) { return memos.splice(index, 1); }
+        const index = memos.indexOf(memo)
+        if (index !== -1) { return memos.splice(index, 1) }
       })
-      .catch(() => { throw Error('unimplemented'); });
-  };
+      .catch(() => { throw Error('unimplemented') })
+  }
 
-  return $scope;
-}]);
+  return $scope
+}])
