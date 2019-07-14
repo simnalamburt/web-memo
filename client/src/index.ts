f843f35ab09853c407b0d4f3ae2a26151a048ae0
Hyeon Kim <simnalamburt@gmail.com>
2019-07-14 21:11:56 +0900

    Use lightweight version of fontawesome

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,12 +1,18 @@
 import angular from 'angular'
 import 'restangular'
 import 'angular-elastic'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes'
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt'
+
 import MainController from './main.controller'
 
 // Stylesheets
-import '@fortawesome/fontawesome-free/css/all.css'
 import 'normalize.css'
 import './index.scss'
+
+library.add(faTimes, faPencilAlt)
+dom.watch()
 
 // Angular module definition
 angular
