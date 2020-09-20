e394e41437b16be4f1632cea0b74c5491d802c74
Hyeon Kim <simnalamburt@gmail.com>
2020-09-21 07:52:22 +0900

    server: WIP

--- server/index.js
+++ server/index.js
@@ -3,7 +3,18 @@ const fastify = require('fastify')
 const server = fastify({ logger: true })
 
 server.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  return [
+    {
+      id: 1,
+      content:
+        'Hello, World!\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in bibendum lorem. In viverra erat ipsum, id pretium urna vehicula ut. Proin vel quam ultricies, placerat urna ut, accumsan leo. Vivamus laoreet vestibulum nulla non dictum. Vestibulum non nisl quis risus dictum vestibulum hendrerit ut diam. Sed eget laoreet augue. Integer pulvinar massa scelerisque rhoncus consequat. Vivamus velit mi, suscipit bibendum tincidunt quis, pulvinar a ante.',
+    },
+    {
+      id: 2,
+      content:
+        'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
+    },
+  ]
 })
 
 server.listen(9494)
