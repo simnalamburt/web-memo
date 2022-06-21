413b5d75bb03ce051bb080f22757fe5f76d4d525
Hyeon Kim <simnalamburt@gmail.com>
2022-06-22 03:25:29 +0900

    (misc) Add a TODO

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,6 +4,7 @@ const fastifyCors = require('fastify-cors')
 const server = fastify({ logger: true })
 server.register(fastifyCors, { origin: 'http://localhost:3000' })
 
+// TODO: Use SQLite3
 let lastId = 2
 const memos = new Map([
   [
